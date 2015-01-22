7be8ddee04be1fe710d095053ef579d7d842ff83
David LeGare <excaliburHisSheath@gmail.com>
2015-01-22 10:49:23 -0600

    FoW Generates a Ring of Colliders

--- Assets/Scripts/FogOfWar.cs
+++ Assets/Scripts/FogOfWar.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class FogOfWar : MonoBehaviour
 {
 	public GameObject cloudPrefab;
+	public GameObject colliderPrefab;
 
 	public float radius;
 	public float radiusError;
@@ -17,6 +18,7 @@ public class FogOfWar : MonoBehaviour
 	public float maxScale;
 
 	public int numClouds;
+	public int numColliders;
 
 	private new Transform transform;
 
@@ -45,6 +47,24 @@ public class FogOfWar : MonoBehaviour
 			                                Random.Range( minScale, maxScale ),
 			                                Random.Range( minScale, maxScale ) );
 			cloud.SetParent( transform, false );
+		}
+
+		// build colliders
+		float increment = 360.0f / numColliders;
+		baseOffset = Vector3.forward;
+		rotation = Quaternion.Euler( 0.0f, increment, 0.0f );
+		for ( int count = 0; count < numColliders; ++count )
+		{
+			baseOffset = rotation * baseOffset;
+
+			Transform collider = ( GameObject.Instantiate(
+				colliderPrefab,
+				baseOffset * radius + Vector3.up * height * 0.5f,
+				Quaternion.LookRotation( baseOffset )
+				) as GameObject ).GetComponent<Transform>();
+			float width = 2 * radius * Mathf.Sin( Mathf.Deg2Rad * increment * 0.5f );
+			collider.localScale = new Vector3( width, height, 1.0f );
+			collider.SetParent( transform, false );
 		}
 	}
 
