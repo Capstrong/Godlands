85525135def06ff3d73bed92f9cb6fb74115df02
David LeGare <excaliburHisSheath@gmail.com>
2015-01-22 09:52:18 -0600

    More Clouds - Clouds spawn at increments instead of randomly. - Clouds spawn at random height. - Added some slight radius variation to give the clouds more depth.

--- Assets/Scripts/FogOfWar.cs
+++ Assets/Scripts/FogOfWar.cs
@@ -7,7 +7,14 @@ public class FogOfWar : MonoBehaviour
 	public GameObject cloudPrefab;
 
 	public float radius;
+	public float radiusError;
 	public float height;
+
+	[Range( 0.0f, 360.0f )]
+	public float cloudIncrement;
+
+	public float minScale;
+	public float maxScale;
 
 	public int numClouds;
 
@@ -20,16 +27,23 @@ public class FogOfWar : MonoBehaviour
 
 	void Start()
 	{
+		Vector3 baseOffset = Vector3.forward;
+		Quaternion rotation = Quaternion.Euler( 0.0f, cloudIncrement, 0.0f );
 		// generate a ring of the cloud prefabs
 		for ( int count = 0; count < numClouds; ++count )
 		{
-			Vector3 cloudOffset = Random.insideUnitCircle.normalized;
-			cloudOffset.z = cloudOffset.y;
-			cloudOffset.y = 0;
+			// rotate base offset and calculate full offset
+			baseOffset = rotation * baseOffset;
+			Vector3 cloudOffset = baseOffset * ( radius + Random.Range( 0, radiusError ) )
+				+ Vector3.up * Random.Range( 0.0f, height );
+
 			Transform cloud = ( GameObject.Instantiate(
 				cloudPrefab,
-				cloudOffset * radius,
+				cloudOffset ,
 				Quaternion.identity ) as GameObject ).GetComponent<Transform>();
+			cloud.localScale = new Vector3( Random.Range( minScale, maxScale ),
+			                                Random.Range( minScale, maxScale ),
+			                                Random.Range( minScale, maxScale ) );
 			cloud.SetParent( transform, false );
 		}
 	}
