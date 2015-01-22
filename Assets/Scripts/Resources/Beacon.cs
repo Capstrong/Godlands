0257242a880be241496c23cfa0fc0feb3e8600df
Chris Wade <cjacobwade@gmail.com>
2015-01-22 02:17:21 -0600

    Setting up resource scroller for garden, adding resource data, reorganizing file hierarchy, adjusting resource spawning

---
+++ Assets/Scripts/Resources/Beacon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beacon : MonoBehaviour 
+{
+	void Update () 
+	{
+		transform.LookAt(Vector3.up);
+		transform.rotation *= Quaternion.Euler(90f, 0f, 0f);
+	}
+}
---
+++ Assets/Scripts/Resources/resourceSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resourceSpawner : MonoBehaviour 
+{
+	public GameObject resourcePrefab;
+	[SerializeField] GameObject beaconPrefab;
+	
+	public int total;
+	public float radius;
+	
+	// Use this for initialization
+	void Start () {
+		
+		for (int i = 0; i < total; i++)
+		{
+			Vector3 position = transform.position + Random.insideUnitSphere * radius;
+			position.y = 0;
+			
+			GameObject resource = WadeUtils.Instantiate(resourcePrefab, position, Quaternion.identity);
+			resource.AddComponent<rotator>();
+
+			GameObject beacon = WadeUtils.Instantiate(beaconPrefab);
+			beacon.transform.parent = resource.transform;
+			WadeUtils.ResetTransform(beacon.transform, true);
+		}
+	}
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawIcon(transform.position + Vector3.up * .5f, "S.png", true);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
