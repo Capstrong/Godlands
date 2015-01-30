b5d945039028919d6c9bfd4bc062be50c63994c1
Zac <brickman1444@gmail.com>
2015-01-29 19:30:56 -0600

    Buddy gameplay. Resource decay and random names.

--- Assets/Scripts/Actors/BuddyStats.cs
+++ Assets/Scripts/Actors/BuddyStats.cs
@@ -3,10 +3,49 @@ using System.Collections;
 
 public class BuddyStats : MonoBehaviour
 {
-	int currentStats = 0;
+	enum StatNames
+	{
+		Invalid,
+		Stamina,
+	}
+
+	[SerializeField] float decreaseResourcesTime = 60.0f; //seconds
+	[SerializeField] float currResourceTimer = 0.0f;
+
+	[SerializeField] int apples = 0;
+
+	void Awake()
+	{
+		name = "Buddy " + GetRandomName();
+		currResourceTimer = decreaseResourcesTime;
+	}
+
+	static string[] names = { "Longnose", "Jojo", "JillyJane", "Sunshine", "Moosejaw"};
+	static uint uniqueID = 1;
+
+	static string GetRandomName()
+	{
+		int randIndex = Random.Range(0, names.Length);
+
+		return names[randIndex] + uniqueID++;
+	}
+
+	void Update()
+	{
+		currResourceTimer -= Time.deltaTime;
+
+		if (currResourceTimer < 0.0f)
+		{
+			// decrease resources
+			apples--;
+			currResourceTimer = decreaseResourcesTime;
+		}
+	}
 
 	public void GiveResource(ActorPhysics actorPhysics, ResourceData resourceData)
 	{
-		currentStats++;
+		apples++;
 	}
+
+
 }
