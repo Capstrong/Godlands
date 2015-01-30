90ef1aee2b97a2897ed9a60522477303bbe1df75
David LeGare <excaliburHisSheath@gmail.com>
2015-01-30 09:52:38 -0600

    Buddies Emote When Given Resources - Buddies emit a single heart icon when given a resource.

--- Assets/Scripts/Actors/BuddyStats.cs
+++ Assets/Scripts/Actors/BuddyStats.cs
@@ -3,10 +3,26 @@ using System.Collections;
 
 public class BuddyStats : MonoBehaviour
 {
-	int currentStats = 0;
+	public Material heartMaterial;
 
-	public void GiveResource(ActorPhysics actorPhysics, ResourceData resourceData)
+	int currentStats = 0;
+	ParticleSystem _particles;
+
+	void Awake()
+	{
+		_particles = GetComponentInChildren<ParticleSystem>();
+	}
+
+	public void GiveResource( ActorPhysics actorPhysics, ResourceData resourceData )
 	{
 		currentStats++;
+		Emote( heartMaterial );
+	}
+
+	public void Emote( Material emoteMaterial )
+	{
+		_particles.Clear();
+		_particles.renderer.material = emoteMaterial;
+		_particles.Emit( 1 );
 	}
 }
