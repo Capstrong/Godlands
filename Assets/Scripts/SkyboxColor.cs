2868e0a808cb3cfaab75c132fe2b0f013a89136b
cjacobwade <cjacobwade@gmail.com>
2015-02-04 14:44:06 -0600

    HSV color utility added to wade utils, skybox test script added

---
+++ Assets/Scripts/SkyboxColor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxColor : MonoBehaviour 
+{
+	[SerializeField] Color startColor;
+	[SerializeField] Color endColor;
+
+	[SerializeField] float shiftTime = 5f;
+	float shiftTimer = 0f;
+
+	void Update()
+	{
+		if(shiftTimer < shiftTime)
+		{
+			Color lerpColor = Color.Lerp( startColor, endColor, shiftTimer/shiftTime );
+			//Color lerpColor = WadeUtils.HSVToRGB( HSVColor.Lerp( new HSVColor( startColor ), new HSVColor( endColor ), shiftTimer/shiftTime) );
+			RenderSettings.skybox.SetColor("_Tint", lerpColor);
+			RenderSettings.fogColor = lerpColor;
+
+			shiftTimer += Time.deltaTime;
+		}
+		else
+		{
+			shiftTimer = 0f;
+
+			Color tempColor = startColor;
+			startColor = endColor;
+			endColor = tempColor;
+		}
+	}
+}
