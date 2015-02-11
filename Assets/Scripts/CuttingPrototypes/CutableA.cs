0a0d23ccca896cdacc811952418cade9961feaf8
Zac <brickman1444@gmail.com>
2015-02-11 11:11:57 -0600

    First cutable type

---
+++ Assets/Scripts/CuttingPrototypes/CutableA.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutableA : MonoBehaviour
+{
+	[SerializeField, Range( 0,30 )]
+	float _level = 0;
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/CuttingPrototypes/CuttingA.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CuttingA : MonoBehaviour
+{
+
+	[SerializeField] LayerMask _cutableLayer = 0;
+	[SerializeField] KeyCode _cuttingButton = (KeyCode) 0;
+	[SerializeField] float _cuttingDistance = 0f;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if ( Input.GetKeyDown( _cuttingButton ) )
+		{
+			RaycastHit hitInfo;
+			Physics.Raycast( new Ray( transform.position, transform.forward ), out hitInfo, _cuttingDistance, _cutableLayer );
+
+			GameObject cutableObj = hitInfo.collider.gameObject;
+
+			CutableA cutableComponent = cutableObj.GetComponent<CutableA>();
+
+			if ( cutableComponent )
+			{
+				Cut( cutableComponent );
+			}
+			else
+			{
+				Debug.LogError( "Attach Cutable component to " + cutableObj + " at " + cutableObj.transform.position );
+			}
+		}
+	}
+
+	void Cut( CutableA cutableComponent )
+	{
+
+	}
+}
