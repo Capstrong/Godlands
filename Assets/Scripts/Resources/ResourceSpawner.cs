5c59b302f1cbf4f008e0ed04e3e3c760121d2e48
Zac <brickman1444@gmail.com>
2015-02-14 17:22:50 -0600

    Fixing parent relationships in resource spawner

--- Assets/Scripts/Resources/ResourceSpawner.cs
+++ Assets/Scripts/Resources/ResourceSpawner.cs
@@ -11,30 +11,35 @@ public class ResourceSpawner : MonoBehaviour
 	public int total;
 	public float radius;
 
+	private new Transform transform = null;
+
 	void Start()
 	{
 		// configure resource holder for the current resource type
 		resourceHolderPrefab.resource = resourcePrefab;
 
+		transform = GetComponent<Transform>();
+
 		// spawn resource holders
 		for ( int i = 0; i < total; i++ )
 		{
-			Vector3 offset = Quaternion.Euler( -90.0f, 0.0f, 0.0f ) * (Vector3)Random.insideUnitCircle;
-			Vector3 position = transform.position + offset * radius;
+			Vector3 offset = Quaternion.Euler( -90.0f, 0.0f, 0.0f ) * (Vector3) Random.insideUnitCircle;
+			Vector3 spawnPosition = transform.position + offset * radius;
 
-			RaycastHit hit = WadeUtils.RaycastAndGetInfo( new Ray( position + Vector3.up, Vector3.down ), groundCheckDist );
+			RaycastHit hit = WadeUtils.RaycastAndGetInfo( new Ray( spawnPosition + Vector3.up, Vector3.down ), groundCheckDist );
 			if( hit.transform && resourcePrefab.GetComponent<Collider>() )
 			{
 				// change position to ray hit pos with offset depending on resource size
-				position = hit.point + hit.normal * resourcePrefab.collider.bounds.size.y;
+				spawnPosition = hit.point + hit.normal * resourcePrefab.collider.bounds.size.y;
 			}
 
-			Instantiate( resourceHolderPrefab, position, Quaternion.identity );
+			ResourceHolder resourceHolderInstance = (ResourceHolder) Instantiate( resourceHolderPrefab, spawnPosition, Quaternion.identity );
+			resourceHolderInstance.gameObject.transform.parent = transform;
 		}
 	}
 
 	void OnDrawGizmos()
 	{
-		Gizmos.DrawIcon(transform.position + Vector3.up * .5f, "ResourceSpawner.png", true);
+		Gizmos.DrawIcon( GetComponent<Transform>().position + Vector3.up * .5f, "ResourceSpawner.png", true );
 	}
 }
