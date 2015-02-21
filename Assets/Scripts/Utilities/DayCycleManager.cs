d55c2e44510a0eeff83ad127c59f90056d704bf2
Zac <brickman1444@gmail.com>
2015-02-20 20:29:37 -0600

    Separately ticking day timer

--- Assets/Scripts/Utilities/DayCycleManager.cs
+++ Assets/Scripts/Utilities/DayCycleManager.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class DayCycleManager : SingletonBehaviour<DayCycleManager> {
 
-	[SerializeField,DisplayOnly]
-	float _lightLevel = 0f;
-	public float lightLevel
+	[DisplayOnly,Tooltip("0 is midnight. 1 is noon.")]
+	[SerializeField] float _daylightIntensity = 1.0f;
+	public float daylightIntensity
 	{
 		get
 		{
-			return _lightLevel;
+			return _daylightIntensity;
 		}
 		private set
 		{
-			_lightLevel = value;
+			_daylightIntensity = value;
 		}
 	}
+
+	[Tooltip( "The length (in seconds) of a day" )]
+	[SerializeField] float _dayCycleLength = 60;
+	[DisplayOnly]
+	[SerializeField] float _dayCycleTimer = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -24,6 +29,13 @@ public class DayCycleManager : SingletonBehaviour<DayCycleManager> {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		_dayCycleTimer += Time.deltaTime;
+
+		daylightIntensity = Mathf.Cos( _dayCycleTimer / _dayCycleLength * 2 * Mathf.PI ) * 0.5f + 0.5f;
+
+		if ( _dayCycleTimer > _dayCycleLength )
+		{
+			_dayCycleTimer -= _dayCycleLength;
+		}
 	}
 }
