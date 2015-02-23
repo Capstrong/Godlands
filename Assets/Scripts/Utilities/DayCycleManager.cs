93dfbce5606d84d39c7cbfcf8f8d39608310dd58
Zac <brickman1444@gmail.com>
2015-02-22 20:00:11 -0600

    Fade in black overlay when midnight approaches

--- Assets/Scripts/Utilities/DayCycleManager.cs
+++ Assets/Scripts/Utilities/DayCycleManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DayCycleManager : SingletonBehaviour<DayCycleManager> 
 {
@@ -9,6 +10,7 @@ public class DayCycleManager : SingletonBehaviour<DayCycleManager>
 	[SerializeField] float _dayCycleLength = 60.0f;
 
 	[SerializeField] float _dayStartTime = 0f;
+	[SerializeField] float _blackOutStartTime = 0f;
 
 	public static float dayCycleTimer { get { return instance._dayCycleTimer; } }
 
@@ -16,6 +18,12 @@ public class DayCycleManager : SingletonBehaviour<DayCycleManager>
 	[SerializeField] float _dayCycleTimer = 0f;
 
 	[SerializeField] PlayerControls _playerControls = null;
+
+	[SerializeField] Image _midnightOverlay = null;
+
+	bool _hasStartedMidnightOverlay = false;
+
+	Coroutine _midnightOverlayCoroutine = null;
 
 	void Start()
 	{
@@ -26,11 +34,47 @@ public class DayCycleManager : SingletonBehaviour<DayCycleManager>
 	void Update () {
 		_dayCycleTimer += Time.deltaTime;
 
+		if ( !_hasStartedMidnightOverlay && _dayCycleTimer > _blackOutStartTime )
+		{
+			StartMidnightOverlay();
+		}
+
 		if ( _dayCycleTimer > _dayCycleLength )
 		{
 			BuddyManager.DecrementAllBuddyResources();
 			_playerControls.Respawn();
 			_dayCycleTimer = _dayCycleTimer - _dayCycleLength + _dayStartTime; // Take overflow into account
+			EndMidnightOverlay();
 		}
+	}
+
+	void StartMidnightOverlay()
+	{
+		_hasStartedMidnightOverlay = true;
+		_midnightOverlayCoroutine = StartCoroutine( FadeInMidnightOverlay() );
+	}
+
+	void EndMidnightOverlay()
+	{
+		_hasStartedMidnightOverlay = false;
+
+		StopCoroutine( _midnightOverlayCoroutine );
+
+		Color color = _midnightOverlay.color;
+		color.a = 0.0f;
+		_midnightOverlay.color = color;
+	}
+
+	IEnumerator FadeInMidnightOverlay()
+	{
+		while ( true )
+		{
+			Color color = _midnightOverlay.color;
+			color.a = ( _dayCycleTimer - _blackOutStartTime ) / ( _dayCycleLength - _blackOutStartTime );
+			_midnightOverlay.color = color;
+			yield return null;
+		}
+
+
 	}
 }
