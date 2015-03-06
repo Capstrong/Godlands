bf955a1dde33e1c0c00176b67879ac18a986f4bc
David LeGare <excaliburHisSheath@gmail.com>
2015-03-06 15:50:48 -0600

    Fix GetComponent() and Shaders - Removed all of the automatically added instances of GetComponent(). - Fixed warnings that were being generated by incorrect shaders.

--- Assets/Scripts/Resources/ResourceSpawner.cs
+++ Assets/Scripts/Resources/ResourceSpawner.cs
@@ -43,7 +43,7 @@ public class ResourceSpawner : MonoBehaviour
 				}
 				
 				// change position to ray hit pos with offset depending on resource size
-				spawnPosition = hit.point + hit.normal * resourcePrefab.GetComponent<Collider>().bounds.size.y;
+				spawnPosition = hit.point + hit.normal * resourcePrefab.bounds.size.y;
 
 				break;
 			}
--- Assets/Scripts/Tags/BuddyLimitsTag.cs
+++ Assets/Scripts/Tags/BuddyLimitsTag.cs
@@ -1,13 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[ExecuteInEditMode]
 public class BuddyLimitsTag : MonoBehaviour
 {
-	void Update()
-	{
-#if UNITY_EDITOR
-		DebugUtils.Assert( GetComponent<Collider>() is CapsuleCollider, "Buddy limits must use capsule collider." );
-#endif // UNITY_EDITOR
-	}
 }
