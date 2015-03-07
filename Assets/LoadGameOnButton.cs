148ee421c2184832aa334766bfa69a39ca917fa7
Zac <brickman1444@gmail.com>
2015-03-07 11:26:13 -0600

    Add level abstraction and disable the image immediately

---
+++ Assets/LoadGameOnButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGameOnButton : MonoBehaviour
+{
+	[SerializeField] string _buttonName = "";
+	[SerializeField] Level _level;
+	
+	Button _button;
+
+	void Start()
+	{
+		_button = new Button( _buttonName );
+	}
+
+	void Update()
+	{
+		_button.Update();
+
+		if ( _button )
+		{
+			gameObject.SetActive( false ); // Immediately show a black screen. The actual level may take a bit to load
+			LevelUtils.LoadLevel( _level );
+		}
+	}
+}
---
+++ Assets/Scripts/Utilities/LevelUtils.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Level
+{
+	Invalid,
+	ControlsScreen,
+	MainGame,
+}
+
+public static class LevelUtils
+{
+	public static string[] levels = { "",
+	                               "controlsScreen",
+	                               "BlockOut"};
+
+	public static void LoadLevel( Level level )
+	{
+		if ( level != Level.Invalid )
+		{
+			Application.LoadLevel( levels[(int)level] );
+		}
+		else
+		{
+			Debug.LogError( "Trying to load invalid level." );
+		}
+	}
+}
