d31cbac5a73dedc2fdf6a2e19010308753895e9e
Zac <brickman1444@gmail.com>
2015-03-07 14:31:45 -0600

    Accidentally tried to put this in the behavior tree submodule. Putting it back now.

---
+++ Assets/Scripts/Buddy/BuddyInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * @brief Holder container for info used by the behavior tree nodes.
+ */
+public class BuddyInfo : MonoBehaviour
+{
+	public float minIdleTime;
+	public float maxIdleTime;
+	public float minIdleWalkRadius;
+	public float maxIdleWalkRadius;
+}
