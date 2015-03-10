a60c0eb752a07aba23f4df0460495260cf996434
David LeGare <excaliburHisSheath@gmail.com>
2015-03-10 15:10:40 -0500

    Turn Night Lights On and Off

--- Assets/Scripts/Tags/NightLightTag.cs
+++ Assets/Scripts/Tags/NightLightTag.cs
@@ -3,4 +3,34 @@ using System.Collections;
 
 public class NightLightTag : MonoBehaviour
 {
+	[Range( 0.0f, 1.0f )]
+	[SerializeField] float _timeOn = 0.6f;
+
+	private Light _light;
+
+	void Awake()
+	{
+		_light = GetComponent<Light>();
+	}
+
+	void Start()
+	{
+		DayCycleManager.RegisterEndOfDayCallback( ResetLight );
+		ResetLight();
+	}
+
+	void ResetLight()
+	{
+		_light.enabled = false;
+
+		// calculate time until the light
+		// needs to turn on again.
+		float timeToOn = DayCycleManager.dayCycleLength * _timeOn - DayCycleManager.currentTime;
+		Invoke( "EnableLight", timeToOn );
+	}
+
+	void EnableLight()
+	{
+		_light.enabled = true;
+	}
 }
