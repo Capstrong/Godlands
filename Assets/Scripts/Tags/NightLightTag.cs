47c9ad3115ae2cf51352e1d0420955975c65adbc
David LeGare <excaliburHisSheath@gmail.com>
2015-03-11 16:12:16 -0500

    Have Night Lights Fade In - Move teleport scripts to Scripts folder. - Move Lights under root in BlockOut scene.

--- Assets/Scripts/Tags/NightLightTag.cs
+++ Assets/Scripts/Tags/NightLightTag.cs
@@ -5,12 +5,15 @@ public class NightLightTag : MonoBehaviour
 {
 	[Range( 0.0f, 1.0f )]
 	[SerializeField] float _timeOn = 0.6f;
+	[SerializeField] float _fadeTime = 1.0f;
 
 	private Light _light;
+	private float _lightIntensity;
 
 	void Awake()
 	{
 		_light = GetComponent<Light>();
+		_lightIntensity = _light.intensity;
 	}
 
 	void Start()
@@ -40,5 +43,17 @@ public class NightLightTag : MonoBehaviour
 	void EnableLight()
 	{
 		_light.enabled = true;
+		_light.intensity = 0.0f;
+
+		StartCoroutine( FadeLight() );
+	}
+
+	IEnumerator FadeLight()
+	{
+		while ( _light.intensity < _lightIntensity )
+		{
+			_light.intensity += _lightIntensity * Time.deltaTime / _fadeTime;
+			yield return null;
+		}
 	}
 }
