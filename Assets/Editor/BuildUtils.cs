101b2056bb97d4caa80dce5449a3f4a6ce48835a
David LeGare <excaliburHisSheath@gmail.com>
2015-03-10 19:12:33 -0500

    Build System - Turned the build settings into an asset file.

--- Assets/Editor/BuildUtils.cs
+++ Assets/Editor/BuildUtils.cs
@@ -1,33 +1,129 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Linq;
 using UnityEditor;
 
-public class BuildUnityPlayer : MonoBehaviour
+public class BuildUnityPlayer : ScriptableObject
 {
-	[MenuItem( "Export/Build All" )]
-	public static void PerformBuild()
+	public string gameName; // TODO get this automatically from project name or something?
+	public Version version;
+	public BuildSettings[] buildSettings;
+	public string[] scenes; // TODO create a more safe system for selecting scenes.
+
+	[HideInInspector]
+	[SerializeField] Version _lastVersion;
+
+	public void PerformBuild()
 	{
-		// the scenes we want to include in the build
-		string[] scenes = { "Scenes/BlockOut.unity",
-		                    "Scenes/ControlsScreen.unity"};
+		// reset patch number if major or minor was changed
+		if ( version.major != _lastVersion.major || version.minor != _lastVersion.minor )
+		{
+			version.patch = 0;
+		}
 
-		DateTime currentDate = DateTime.Now;
-		string buildName = "Godlands_" + currentDate.ToShortDateString();
+		foreach ( BuildSettings build in buildSettings )
+		{
+			string buildName = gameName + "_" + version + "_" + BuildSuffix( build.target );
+			EditorUserBuildSettings.SwitchActiveBuildTarget( build.target );
+			BuildPipeline.BuildPlayer(
+				( from scene in scenes select "Assets/Scenes/" + scene + ".unity" ).ToArray<string>(),
+				"Build/" + buildName + "/" + gameName + BuildExtension( build.target ),
+				build.target,
+				build.options );
+		}
 
-		// build for windows stand alone
-		string windowsStandAloneBuildName = buildName + "_WIN.exe";
-		EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.StandaloneWindows );
-		BuildPipeline.BuildPlayer( scenes, windowsStandAloneBuildName, BuildTarget.StandaloneWindows, BuildOptions.None );
+		_lastVersion = version;
+		version.patch++;
+	}
 
-		// build for windows stand alone
-		string macStandAloneBuildName = buildName + "_OSX.exe";
-		EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.StandaloneOSXUniversal );
-		BuildPipeline.BuildPlayer( scenes, macStandAloneBuildName, BuildTarget.StandaloneOSXUniversal, BuildOptions.None );
+	/**
+	 * @todo Add suffixes for different targets.
+	 */
+	static string BuildSuffix( BuildTarget target )
+	{
+		switch ( target )
+		{
+			case BuildTarget.StandaloneWindows:
+			case BuildTarget.StandaloneWindows64:
+				return "WIN";
+			case BuildTarget.StandaloneOSXIntel:
+			case BuildTarget.StandaloneOSXIntel64:
+			case BuildTarget.StandaloneOSXUniversal:
+				return "OSX";
+			case BuildTarget.StandaloneLinux:
+			case BuildTarget.StandaloneLinux64:
+			case BuildTarget.StandaloneLinuxUniversal:
+				return "LINUX";
+			default:
+				return "BUILD";
+		}
+	}
 
-		// build for windows stand alone
-		string linuxStandAloneBuildName = buildName + "_LINUX.exe";
-		EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.StandaloneLinuxUniversal );
-		BuildPipeline.BuildPlayer( scenes, linuxStandAloneBuildName, BuildTarget.StandaloneLinuxUniversal, BuildOptions.None );
+	static string BuildExtension( BuildTarget target )
+	{
+		switch ( target )
+		{
+			case BuildTarget.StandaloneWindows:
+			case BuildTarget.StandaloneWindows64:
+				return ".exe";
+			case BuildTarget.StandaloneOSXIntel:
+			case BuildTarget.StandaloneOSXIntel64:
+			case BuildTarget.StandaloneOSXUniversal:
+				return ".app";
+			case BuildTarget.StandaloneLinux:
+			case BuildTarget.StandaloneLinux64:
+			case BuildTarget.StandaloneLinuxUniversal:
+				return ".LINUX"; // TODO what even is
+			default:
+				return ".build";
+		}
+	}
+
+	/**
+	 * @brief Add a menu item in the editor for creating new behavior tree assets.
+	 *
+	 * @todo
+	 *     Create new asset in currently selected folder of the project view,
+	 *     rather than always placing them in the root Assets folder.
+	 *
+	 * @todo
+	 *     Check if asset already exists, because by default the AssetDatabase
+	 *     will overwrite an existing asset of the same name.
+	 */
+	[MenuItem( "Assets/Create/Build Settings" )]
+	public static void CreateBehaviorTreeAsset()
+	{
+		BuildUnityPlayer buildAsset = ScriptableObject.CreateInstance<BuildUnityPlayer>();
+		AssetDatabase.CreateAsset( buildAsset, "Assets/BuildSettings.asset" );
+		AssetDatabase.SaveAssets();
+
+		EditorUtility.FocusProjectWindow();
+		Selection.activeObject = buildAsset;
+	}
+}
+
+[Serializable]
+public struct BuildSettings
+{
+	public BuildTarget target;
+	public BuildOptions options;
+}
+
+[Serializable]
+public struct Version
+{
+	public int major;
+	public int minor;
+	[HideInInspector]
+	public int patch;
+	public string extra;
+
+	public static implicit operator string ( Version version )
+	{
+		return version.major + "." +
+		       version.minor + "." +
+		       version.patch +
+		       ( version.extra != "" ? "-" + version.extra : "" );
 	}
 }
