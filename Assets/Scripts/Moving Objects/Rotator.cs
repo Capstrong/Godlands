e5301f310d79b24b9b7e8f862b79316924191c0d
David LeGare <excaliburHisSheath@gmail.com>
2015-03-12 16:27:19 -0500

    Simple Planet Clouds - Clouds are done as two semi-transparent meshes around the planet.

--- Assets/Scripts/Moving Objects/Rotator.cs
+++ Assets/Scripts/Moving Objects/Rotator.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour 
 {
-	[SerializeField] Vector3 eulerRotation = Vector3.zero;
+	[Tooltip("Rate of rotation around each axis in degrees per second.")]
+	[SerializeField] Vector3 _rotation = Vector3.zero;
 
 	void Update () 
 	{
-		transform.rotation *= Quaternion.Euler( eulerRotation * Time.deltaTime );
+		transform.rotation *= Quaternion.Euler( _rotation * Time.deltaTime );
 	}
 }
