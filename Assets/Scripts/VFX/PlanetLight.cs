abe4e7843cd479ab9bd5fe762f280ce44a3f478f
David LeGare <excaliburHisSheath@gmail.com>
2015-03-12 18:33:32 -0500

    Spinning Diffuse - Planet shader now has a light direction and only does the rim glow on the lit side of the planet. - Added PlanetLight.cs to slowly spin the planet's light direction.

---
+++ Assets/Scripts/VFX/PlanetLight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetLight : MonoBehaviour
+{
+	[Tooltip( "Rate of ratation in degrees per second. Rotation is around the y axis." )]
+	[SerializeField] float _rotationSpeed = 0.1f;
+
+	private Vector4 _lightDirection;
+	private Renderer _renderer;
+
+	void Awake()
+	{
+		_renderer = GetComponent<Renderer>();
+		_lightDirection = _renderer.material.GetVector("_LightDir");
+	}
+
+	void Update()
+	{
+		Quaternion rotation = Quaternion.Euler( 0.0f, _rotationSpeed * Time.deltaTime, 0.0f );
+		_lightDirection = rotation * _lightDirection;
+		_renderer.material.SetVector(
+			"_LightDir",
+			new Vector4( _lightDirection.x, _lightDirection.y, _lightDirection.z ) );
+	}
+}
