d312bfd8fffa8d03c2c1e11ccc19e73d6fbf3225
Zac <brickman1444@gmail.com>
2015-03-16 20:03:38 -0500

    Resource curve struct

--- Assets/Scripts/Managers/BuddyManager.cs
+++ Assets/Scripts/Managers/BuddyManager.cs
@@ -2,9 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[System.Serializable]
+public struct BuddyResourceCurve
+{
+	public float a;
+	public float b;
+	public float c;
+
+	public float Evaluate( float numBuddies )
+	{
+		return a * Mathf.Pow( numBuddies, b ) + c;
+	}
+}
+
 public class BuddyManager : SingletonBehaviour<BuddyManager> {
 
 	List<BuddyStats> _buddyStats = new List<BuddyStats>();
+
+	// total resources needed per buddy type = a * ( # buddies of that type) ^ b + c
+	[SerializeField] BuddyResourceCurve _resourceCurve;
 
 	void Start()
 	{
