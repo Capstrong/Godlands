6a0eda6b821ecbb9dd5fe1fb3287c1b045526b53
David LeGare <excaliburHisSheath@gmail.com>
2015-03-20 17:16:54 -0500

    Code Review Changes

--- Assets/Scripts/Resources/ResourceHolder.cs
+++ Assets/Scripts/Resources/ResourceHolder.cs
@@ -26,15 +26,15 @@ public class ResourceHolder : MonoBehaviour
 
 		Physics.Raycast( new Ray( _transform.position, Vector3.up ), out hitInfo, _maxHeight, _collisionLayer );
 
-		float normalizedHeight = _maxHeight;
+		float actualHeight = _maxHeight;
 
 		if ( hitInfo.transform )
 		{
-			normalizedHeight = hitInfo.transform.position.y - _transform.position.y;
+			actualHeight = hitInfo.transform.position.y - _transform.position.y;
 		}
 
-		_beacon.localScale = _beacon.localScale.SetY( normalizedHeight );
-		_particleSystem.startLifetime = normalizedHeight / _particleSystem.startSpeed * 0.5f; // when the speed of a particle is 1
+		_beacon.localScale = _beacon.localScale.SetY( actualHeight );
+		_particleSystem.startLifetime = actualHeight / _particleSystem.startSpeed * 0.5f; // TODO find out why particles seem to be moving at 2x speed
 
 		resource = WadeUtils.Instantiate( resource, Vector3.up * resourceHeightOffset, Quaternion.identity );
 		resource.GetComponent<Transform>().SetParent( _transform, false );
