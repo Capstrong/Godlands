9e95ff4574a037cc858a2c160c36b17d204cd0d3
David LeGare <excaliburHisSheath@gmail.com>
2015-04-06 11:34:50 -0500

    Update to Latest Behavior Tree State

--- Assets/Scripts/Nodes/BuddyNodes/PickRandomDestinationInRadius.cs
+++ Assets/Scripts/Nodes/BuddyNodes/PickRandomDestinationInRadius.cs
@@ -5,23 +5,24 @@ using BehaviorTree;
 
 public class PickRandomDestinationInRadius : LeafNode
 {
+	public float minRadius = 5.0f;
+	public float maxRadius = 10.0f;
+
 	private GameObject gameObject;
 	private Transform transform;
 	private BehaviorTreeInfo baseInfo;
-	private BuddyInfo buddyInfo;
 
 	public override void InitSelf( Hashtable data )
 	{
 		gameObject = (GameObject)data["gameObject"];
 		transform = gameObject.GetComponent<Transform>();
 		baseInfo = gameObject.GetComponent<BehaviorTreeInfo>();
-		buddyInfo = gameObject.GetComponent<BuddyInfo>();
 	}
 
 	public override NodeStatus TickSelf()
 	{
-		Vector3 offset = Random.insideUnitCircle
-			* Random.Range( buddyInfo.minIdleWalkRadius, buddyInfo.maxIdleWalkRadius );
+		Vector3 offset =
+			Random.insideUnitCircle * Random.Range( minRadius, maxRadius );
 		offset.z = offset.y;
 		offset.y = 0;
 		baseInfo.destination = transform.position + offset;
