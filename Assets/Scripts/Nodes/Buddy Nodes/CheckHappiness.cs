5a9033bcc7aec2c580bcbdc095a9064390223e78
David LeGare <excaliburHisSheath@gmail.com>
2015-04-12 18:25:18 -0500

    Buddy Actions - Adds some of the framework needed for the buddies to perform different actions.

---
+++ Assets/Scripts/Nodes/Buddy Nodes/CheckHappiness.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using BehaviorTree;
+
+public class CheckHappiness : LeafNode
+{
+	[Range( 0.0f, 1.0f )]
+	public float value;
+	public Comparison comparison;
+
+	private BuddyStats _buddyStats;
+
+	public override void InitSelf( Hashtable data )
+	{
+		GameObject gameObject = (GameObject)data["gameObject"];
+		_buddyStats = gameObject.GetComponent<BuddyStats>();
+	}
+
+	public override NodeStatus TickSelf()
+	{
+		switch ( comparison )
+		{
+		case Comparison.EqualTo:
+			if ( value == _buddyStats.happiness ) return NodeStatus.SUCCESS;
+			break;
+		case Comparison.GreaterThan:
+			if ( value > _buddyStats.happiness ) return NodeStatus.SUCCESS;
+			break;
+		case Comparison.LessThan:
+			if ( value < _buddyStats.happiness ) return NodeStatus.SUCCESS;
+			break;
+		}
+
+		return NodeStatus.FAILURE;
+	}
+}
