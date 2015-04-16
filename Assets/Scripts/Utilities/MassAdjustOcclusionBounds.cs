9305a1a7cfba2ecb21747ca78e5ba29e4ee13ba9
cjacobwade <cjacobwade@gmail.com>
2015-04-16 15:03:41 -0500

    Deleted it

--- Assets/Scripts/Utilities/MassAdjustOcclusionBounds.cs
+++ Assets/Scripts/Utilities/MassAdjustOcclusionBounds.cs
@@ -3,25 +3,5 @@ using System.Collections;
 
 public class MassAdjustOcclusionBounds : MonoBehaviour 
 {
-//	void Start()
-//	{
-//		Bounds bounds = new Bounds();
-//
-//		int counter = 0;
-//		foreach( MeshFilter mf in GameObject.FindObjectsOfType<MeshFilter>() )
-//		{
-//			counter++;
-//			if( counter > 50 )
-//			{
-//				Mesh mesh = mf.mesh;
-//				bounds.center = mesh.bounds.center;
-//				bounds.size = mesh.bounds.size * 2f;
-//
-//				mesh.bounds = bounds;
-//				mf.mesh = mesh;
-//			}
-//		}
-//
-//		Debug.Log( counter );
-//	}
+
 }
