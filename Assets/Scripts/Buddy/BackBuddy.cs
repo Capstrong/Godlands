bd8f9bbec33e32ae5791969dfda2ccbda53f27f9
cjacobwade <cjacobwade@gmail.com>
2015-04-28 15:03:11 -0500

    Placed buddy on back, setup face animations, replacing character with new model

---
+++ Assets/Scripts/Buddy/BackBuddy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackBuddy : MonoBehaviour 
+{
+	Animator animator = null;
+
+	public void PlayEvent( string eventName )
+	{
+		if( !animator )
+		{
+			animator = GetComponent<Animator>();
+		}
+
+		animator.Play( eventName );
+	}
+}
