1bfdc339d3b18c053d2165e2dfdde43a56c3447e
Zac <brickman1444@gmail.com>
2015-05-02 16:25:48 -0500

    Moves the menu item for making a new travel sound player to the menu item script

--- Assets/Scripts/Actors/TravelSoundPlayer.cs
+++ Assets/Scripts/Actors/TravelSoundPlayer.cs
@@ -1,10 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 public class TravelSoundPlayer : ScriptableObject
 {
 	[SerializeField] AudioClip[] _travelSounds;
@@ -17,19 +13,4 @@ public class TravelSoundPlayer : ScriptableObject
 	{
 		return _travelSounds[Random.Range( 0, _travelSounds.Length )];
 	}
-
-#if UNITY_EDITOR
-	[MenuItem( "Assets/Create/Travel Sound Player" )]
-	public static void CreateTravelSoundPlayerAsset()
-	{
-		// Create asset.
-		TravelSoundPlayer travelSoundPlayer = ScriptableObject.CreateInstance<TravelSoundPlayer>();
-		AssetDatabase.CreateAsset( travelSoundPlayer, "Assets/NewTravelSoundPlayer.asset" );
-
-		// Set asset as the active object.
-		AssetDatabase.SaveAssets();
-		EditorUtility.FocusProjectWindow();
-		Selection.activeObject = travelSoundPlayer;
-	}
-#endif
 }
