cec6b8688120c1674efeccfffa28c6e0993b48c1
Zac <brickman1444@gmail.com>
2015-05-30 14:33:41 -0500

    Shuffle the buddy trigger names and store in a static list.

--- Assets/Scripts/Actors/Buddies/CinematicBuddyAnimationTrigger.cs
+++ Assets/Scripts/Actors/Buddies/CinematicBuddyAnimationTrigger.cs
@@ -7,20 +7,38 @@ public class CinematicBuddyAnimationTrigger : MonoBehaviour
 	public string[] _triggerNames = null;
 
 	static int triggerIndex = 0;
+	static string[] _shuffledTriggerNames = null;
 
 	void Start () 
 	{
-		string triggerName = _triggerNames[ triggerIndex ];
+		if ( _shuffledTriggerNames == null )
+		{
+			ShuffleNames();
+		}
+
+		string triggerName = _shuffledTriggerNames[ triggerIndex ];
 
 		Animator animator = GetComponent<Animator>();
 		animator.Play( triggerName );
 
-		// Make it kind of random while also encouraging it to move through the list and not hit the same thing twice
-		triggerIndex += Random.Range( 1, 3 );
+		triggerIndex++;
 
-		if ( triggerIndex >= _triggerNames.Length )
+		if ( triggerIndex >= _shuffledTriggerNames.Length )
 		{
 			triggerIndex = 0;
+		}
+	}
+
+	void ShuffleNames()
+	{
+		_shuffledTriggerNames = _triggerNames;
+		
+		for ( int i = 0; i < _shuffledTriggerNames.Length - 1; i++ )
+		{
+			int randomIndex = Random.Range( i + 1, _shuffledTriggerNames.Length );
+			string temp = _shuffledTriggerNames[ randomIndex ];
+			_shuffledTriggerNames[randomIndex] = _shuffledTriggerNames[i];
+			_shuffledTriggerNames[i] = temp;
 		}
 	}
 }
