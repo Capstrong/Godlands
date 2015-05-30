45dbcc7626419fd91de2e4bf1b84efb82c7f5afe
Zac <brickman1444@gmail.com>
2015-05-29 21:42:14 -0500

    Buddies now get random colors and random animations in the good ending.

---
+++ Assets/Scripts/Actors/Buddies/CinematicBuddyAnimationTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof( Animator ) )]
+public class CinematicBuddyAnimationTrigger : MonoBehaviour
+{
+	public string[] _triggerNames = null;
+
+	static int triggerIndex = 0;
+
+	void Start () 
+	{
+		string triggerName = _triggerNames[ triggerIndex ];
+
+		Animator animator = GetComponent<Animator>();
+		animator.Play( triggerName );
+
+		// Make it kind of random while also encouraging it to move through the list and not hit the same thing twice
+		triggerIndex += Random.Range( 1, 3 );
+
+		if ( triggerIndex >= _triggerNames.Length )
+		{
+			triggerIndex = 0;
+		}
+	}
+}
