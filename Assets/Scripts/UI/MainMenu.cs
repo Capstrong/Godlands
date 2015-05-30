594d717abdf216d6f389bce3f063d70f65ece721
David LeGare <excaliburHisSheath@gmail.com>
2015-05-30 13:00:15 -0500

    Add Main Menu Scene
    - Creates a duplicate of the BlockOut scene for displaying the main menu.
    - Adds a main menu interface for starting the game or exiting.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	public void StartGame()
+	{
+		LevelUtils.LoadLevel( Level.MainGame );
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
