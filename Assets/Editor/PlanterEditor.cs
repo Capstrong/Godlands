06c8eba151d80ae88b71bb203d6cfdb5ab270937
David LeGare <excaliburHisSheath@gmail.com>
2015-06-04 11:05:09 -0500

    Grass Planter Tool
    - Adds a simple tool for automating the placement of grass and other scenery around the level.

---
+++ Assets/Editor/PlanterEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor( typeof( Planter ) )]
+public class PlanterEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		Planter planter = (Planter)target;
+		
+		if ( GUILayout.Button( "Replant" ) )
+		{
+			planter.Plant();
+		}
+	}
+}
