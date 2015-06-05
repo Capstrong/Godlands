ef2950c0b96e322ebf2757d209aae8e72c4331e2
David LeGare <excaliburHisSheath@gmail.com>
2015-06-05 16:58:18 -0500

    Bug Fixes
    - Fixes a bug with TextAdultBuddyVolume where the text box would never go away.
    - Makes the idle animation loop.

--- Assets/Scripts/UI/Tutorial/TextAdultBuddyVolume.cs
+++ Assets/Scripts/UI/Tutorial/TextAdultBuddyVolume.cs
@@ -9,22 +9,34 @@ public class TextAdultBuddyVolume : TextVolume
 	[TextArea( 3, 10 ), Tooltip( "Text to show if the player is NOT carrying an adult buddy" )]
 	[SerializeField] string _noAdultBuddyText = "";
 
+	public float activationDelay = 10.0f;
+	bool _canActivate = true;
+
 	public override void TriggerText( PlayerActor player )
 	{
-		if ( player )
+		if ( player && _canActivate )
 		{
-			if ( player.inventory.backBuddy 
-				&& player.inventory.backBuddy.hiddenBuddy 
+			if ( player.inventory.backBuddy
+				&& player.inventory.backBuddy.hiddenBuddy
 				&& player.inventory.backBuddy.hiddenBuddy.isOfAge )
 			{
 				DisplayText( _adultBuddyText );
 				Reactivate();
+				_canActivate = false;
+				Invoke( "Enable", activationDelay );
 			}
 			else
 			{
 				DisplayText( _noAdultBuddyText );
 				Reactivate();
+				_canActivate = false;
+				Invoke( "Enable", activationDelay );
 			}
 		}
+	}
+
+	void Enable()
+	{
+		_canActivate = true;
 	}
 }
