2a77cf3e599856f770a4d7b0314338a6f92e74d9
Zac <brickman1444@gmail.com>
2015-06-08 17:16:11 -0500

    Freeze the buddy's face on death. Looks like it's not blinking either.

--- Assets/Scripts/Actors/Buddies/BuddyFaceController.cs
+++ Assets/Scripts/Actors/Buddies/BuddyFaceController.cs
@@ -4,27 +4,16 @@ using System.Collections;
 public class BuddyFaceController : MonoBehaviour 
 {
 	Animator _animator = null;
-	Animator animator
-	{
-		get 
-		{
-			if( !_animator )
-			{
-				_animator = GetComponentInParent<Animator>();
-			}
-			
-			return _animator;
-		}
-	}
+
 	void Awake()
 	{
-
+		_animator = GetComponentInParent<Animator>();
 	}
 
 	public void PlayEvent( string eventName )
 	{
 		// Second param here is the animationLayer to play an event on
 		// 0 is the default layer, 1 is the face layer
-		animator.Play( eventName, 1 );
+		_animator.Play( eventName, 1 );
 	}
 }
